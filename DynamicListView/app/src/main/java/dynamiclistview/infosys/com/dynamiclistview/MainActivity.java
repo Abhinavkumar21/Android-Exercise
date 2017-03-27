6d39b99ad96966d91d33a38e260a808fe83a7245
Abhinavkumar21 <abhinav_kumar21@infosys.com>
2017-03-27 22:52:37 +0530

    Performance Tunning
    Done some performance tunning along with removing hardcoding

--- DynamicListView/app/src/main/java/dynamiclistview/infosys/com/dynamiclistview/MainActivity.java
+++ DynamicListView/app/src/main/java/dynamiclistview/infosys/com/dynamiclistview/MainActivity.java
@@ -9,22 +9,12 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.Window;
 import android.widget.TextView;
-
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,6 +23,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Abhinav_Kumar21 on 3/24/2017.
+ *
+ * MainActivity.java - a simple Activity class for entry point of Application.
+ * @author  Alvin Abhinav_Kumar21
+ * @version 1.0
+ * @Description This class is the Activity Class which has Base call for all Related classes - TitleBar, Network,
+ *              RetroFit.
+ *
+ *
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -46,15 +44,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //Request for custom title bar
         this.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+
         //set to your layout file
         setContentView(R.layout.activity_main);
 
+        //Hide the default Titlebar defaults with AppCompatActivity to override with custom Titlebar
         getSupportActionBar().hide();
-        // Lookup the swipe container viewt
+
+        //Set the titlebar layout
+        this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.activity_titlebar);
+
+        //Read the value for setting JSON text in TitleBar
+        tv_titlebar = (TextView)findViewById(R.id.titlebartext);
+
+        // Lookup the swipe container view
         swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
 
+        //pull to refresh functions
         swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -62,30 +71,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Set the titlebar layout
-        this.getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.activity_titlebar);
-        tv_titlebar = (TextView)findViewById(R.id.titlebartext);
-
-        initViews();
+        initializeView();
     }
 
 
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getSystemService(getApplicationContext().CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }
+   /*
+       This refreshes the items on pull to refresh
+    */
 
     void refreshItems() {
         // Load items
         // ...
 
-        loadJSON();
+        retrieveJSONDataGet();
 
         // Load complete
         onItemsLoadComplete();
     }
+
+    /*
+       This completes pull to refresh
+    */
 
     void onItemsLoadComplete() {
         // Update the adapter and notify data set changed
@@ -94,53 +100,51 @@ public class MainActivity extends AppCompatActivity {
         // Stop refresh animation
         swipeContainer.setRefreshing(false);
     }
-    private void initViews(){
+
+    /*
+       This Function initiate and set Views
+       It checks Network Availability and Notify user of connection related Behavior
+     */
+    private void initializeView(){
+
         recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
 
-        if(isNetworkAvailable())
-             loadJSON();
+        if(Util.isNetworkAvailable(getApplicationContext())) {
+            retrieveJSONDataGet();
+        }
         else
         {
-
-            new AlertDialog.Builder(this)
-                    .setTitle("Network Error")
-                    .setMessage("You are not connected to network. Please change mobile network settings and try again.")
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            // continue with delete
-                            finish();
-                        }
-                    })
-                    .show();
+            Util.showErrorMessage(this);
         }
     }
-    private void loadJSON(){
+
+    /*
+       Here initialize Retrofit and carry out network operations.
+       RequestInterface object is created
+       Asyncronous request Call object should be created from RequestInterface object
+       onResponse()/onFailure callback are excuted for Async call
+  */
+    private void retrieveJSONDataGet(){
+
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://dl.dropboxusercontent.com/s/")
+                .baseUrl(Util.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
         RequestInterface request = retrofit.create(RequestInterface.class);
         Call<DataJSON> call = request.getJSON();
         call.enqueue(new Callback<DataJSON>() {
             @Override
             public void onResponse(Call<DataJSON> call, Response<DataJSON> response) {
 
-                System.out.println("onResponse ::" + new Gson().toJson(response.body()));
-
-                //JSONResponse res = response.body();
-
                 DataJSON jsonResponse = response.body();
-                System.out.println("Title ::" + new Gson().toJson(jsonResponse.getTitle()));
-                System.out.println("Title ::" + tv_titlebar);
-                tv_titlebar.setText(jsonResponse.getTitle());
+                tv_titlebar.setText(jsonResponse.getTitle());//setup Titlebar Text
                 data = new ArrayList<>(Arrays.asList(jsonResponse.getRows()));
                 adapter = new DataAdapter(getApplicationContext(),data);
                 recyclerView.setAdapter(adapter);
-
-
             }
 
             @Override
--- DynamicListView/app/src/main/java/dynamiclistview/infosys/com/dynamiclistview/Util.java
+++ DynamicListView/app/src/main/java/dynamiclistview/infosys/com/dynamiclistview/Util.java
@@ -1,9 +1,46 @@
 package dynamiclistview.infosys.com.dynamiclistview;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by Abhinav_Kumar21 on 3/27/2017.
+ * UTIL class contain all user defined data and variables to avoid hardcoding
+ *
  */
 public class Util {
+
+    public static final String BASE_URL = "https://dl.dropboxusercontent.com/s/";
+    public static final String ENDPOINT_URL = "";
+    private static String NETWORK_ERROR_HEADING = "Network Error";
+    private static String NETWORK_ERROR_MESSAGE = "You are not connected to network. Please change mobile network settings and try again.";
+
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(context.getApplicationContext().CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public static void showErrorMessage(final Context context){
+
+        new AlertDialog.Builder(context)
+                .setTitle(NETWORK_ERROR_HEADING)
+                .setMessage(NETWORK_ERROR_MESSAGE)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                    }
+                })
+                .show();
+
+    }
+
 
 
 }
